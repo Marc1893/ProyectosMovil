975783ee3e3d857d6ea70e5929833af0da0f2dcd
Marc1893 <31118198+Marc1893@users.noreply.github.com>
2018-02-11 21:40:23 -0600

    Cliente con Content Provider

---
+++ ClienteContentProvider/app/src/main/java/net/marcrodriguez/clientecontentprovider/activity_datos.java
@@ -0,0 +1,21 @@
+package net.marcrodriguez.clientecontentprovider;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Marc on 11/02/18.
+ */
+
+public class activity_datos extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+
+
+    }
+
+}
